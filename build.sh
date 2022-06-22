ada150719cefd2edceaddd571123771b25404149
longjuan <769022681@qq.com>
2022-06-22 22:25:36 +0800

    加入环境变量DOCKER_NAMESPACE_NAME构建

--- build.sh
+++ build.sh
@@ -1,16 +1,21 @@
 mvn -DskipTests=true clean package
 docker --version
 if [ $? -eq 0 ]; then
+  if [ ${DOCKER_NAMESPACE_NAME} ]; then
+    DOCKER_NAMESPACE_HEAD_STR=${DOCKER_NAMESPACE_NAME}/
+  else
+    DOCKER_NAMESPACE_HEAD_STR=""
+  fi
   cd ./fic-gateway
-  docker build -t fic-gateway:latest ./fic-gateway
+  docker build -t "${DOCKER_NAMESPACE_HEAD_STR}"fic-gateway:latest ./fic-gateway
   cd ../fic-user
-  docker build -t fic-user:latest .
+  docker build -t "${DOCKER_NAMESPACE_HEAD_STR}"fic-user:latest .
   cd ../fic-auth
-  docker build -t fic-auth:latest ./fic-auth
+  docker build -t "${DOCKER_NAMESPACE_HEAD_STR}"fic-auth:latest ./fic-auth
   cd ../fic-kanban
-  docker build -t fic-kanban:latest ./fic-kanban
+  docker build -t "${DOCKER_NAMESPACE_HEAD_STR}"fic-kanban:latest ./fic-kanban
   cd ../fic-search
-  docker build -t fic-search:latest ./fic-search
+  docker build -t "${DOCKER_NAMESPACE_HEAD_STR}"fic-search:latest ./fic-search
 else
     echo "Docker not installed"
 fi
