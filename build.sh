e59f5cc7f1117192d04791258ab45b63ec21644c
longjuan <769022681@qq.com>
2022-06-22 22:31:52 +0800

    加入环境变量DOCKER_IMAGE_NAME构建

--- build.sh
+++ build.sh
@@ -7,15 +7,15 @@ if [ $? -eq 0 ]; then
     DOCKER_NAMESPACE_HEAD_STR=""
   fi
   cd ./fic-gateway
-  docker build -t "${DOCKER_NAMESPACE_HEAD_STR}"fic-gateway:latest ./fic-gateway
+  docker build -t "${DOCKER_NAMESPACE_HEAD_STR}"fic-gateway:${DOCKER_IMAGE_NAME:-latest} ./fic-gateway
   cd ../fic-user
-  docker build -t "${DOCKER_NAMESPACE_HEAD_STR}"fic-user:latest .
+  docker build -t "${DOCKER_NAMESPACE_HEAD_STR}"fic-user:${DOCKER_IMAGE_NAME:-latest} .
   cd ../fic-auth
-  docker build -t "${DOCKER_NAMESPACE_HEAD_STR}"fic-auth:latest ./fic-auth
+  docker build -t "${DOCKER_NAMESPACE_HEAD_STR}"fic-auth:${DOCKER_IMAGE_NAME:-latest} ./fic-auth
   cd ../fic-kanban
-  docker build -t "${DOCKER_NAMESPACE_HEAD_STR}"fic-kanban:latest ./fic-kanban
+  docker build -t "${DOCKER_NAMESPACE_HEAD_STR}"fic-kanban:${DOCKER_IMAGE_NAME:-latest} ./fic-kanban
   cd ../fic-search
-  docker build -t "${DOCKER_NAMESPACE_HEAD_STR}"fic-search:latest ./fic-search
+  docker build -t "${DOCKER_NAMESPACE_HEAD_STR}"fic-search:${DOCKER_IMAGE_NAME:-latest} ./fic-search
 else
     echo "Docker not installed"
 fi
